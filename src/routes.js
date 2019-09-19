32d2038eb5763f02ad11d6ccb1ae9633b7a4d7da
Fumegalli <rafaelxxc@gmail.com>
2019-09-19 13:48:30 -0300

    Fix eslint + sequelize config

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
-import {
-  Router
-} from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+const routes = new Router();
 
 routes.get('/', (req, res) => {
-  res.json('bombou')
-})
+  res.json('bombou');
+});
 
-export default routes
+export default routes;
